1c802c5eeab79673d5cbff4c45108618a4dca973
xthema suporte <xthema.suporte@gmail.com>
2024-08-05 02:17:14 -0300

    remove conf. PORT

--- config/express.js
+++ config/express.js
@@ -7,7 +7,7 @@ const cors = require("cors");
 module.exports = () => {
   const app = express();
   app.use(cors({ origin: "*" }));
-  app.set("port", process.env.PORT);
+  //app.set("port", process.env.PORT);
   app.use(bodyParser.json({ limit: "50mb" }));
 
   consign({ cwd: "api" })
